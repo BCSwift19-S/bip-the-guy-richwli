8b2494c6a5eb4c9cc45225bc4e695124fbacda78
BC Swift Student Loan 1 <bcswiftstudentloan1@csomswiftloan1.local>
2019-02-03 16:52:56 -0500

    Initial Commit

---
+++ Bip the Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip the Guy
+//
+//  Created by BC Swift Student Loan 1 on 2/3/19.
+//  Copyright © 2019 Richard Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
