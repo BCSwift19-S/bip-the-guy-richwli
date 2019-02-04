f182327f7096b9bd41829577f38cdf62567353f8
BC Swift Student Loan 1 <bcswiftstudentloan1@csomswiftloan1.local>
2019-02-04 14:44:40 -0500

    Section 4.1 and 4.2

--- Bip the Guy/ViewController.swift
+++ Bip the Guy/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageToPunch: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    func animateImage(){
+        let bounds = self.imageToPunch.bounds
+        let decreaseBySize: CGFloat = 60
+        self.imageToPunch.bounds = CGRect(x: self.imageToPunch.bounds.origin.x + decreaseBySize, y: self.imageToPunch.bounds.origin.y+decreaseBySize, width:self.imageToPunch.bounds.size.width-decreaseBySize, height: self.imageToPunch.bounds.size.height-decreaseBySize)
+        
+        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2,
+                       initialSpringVelocity: 10, options: [],
+                       animations: {self.imageToPunch.bounds = bounds}, completion: nil)
+    }
+    @IBAction func pressButtonToHit(_ sender: UIButton) {
+        animateImage()
+    }
+    
 }
 
